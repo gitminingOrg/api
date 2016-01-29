33875476e33edf96e0f8a669f237736c3cdff792
Yiming Chen <630346810@qq.com>
2016-01-29 11:25:48 +0800

    serach single repo

--- api/src/main/java/org/gitmining/api/controller/RepositoryController.java
+++ api/src/main/java/org/gitmining/api/controller/RepositoryController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.bson.Document;
 import org.gitmining.api.service.RepositoryInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class RepositoryController {
 	public List<Document> getAllRepos(HttpServletRequest request,
 			HttpServletResponse response){
 		return repositoryInfoService.getAllRepos();
+	}
+	
+	@RequestMapping(value = "/{owner}/{reponame}")
+	public Document getRepo(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable("reponame") String reponame, @PathVariable("owner") String owner){
+		return repositoryInfoService.getRepoInfo(owner+"/"+reponame);
 	}
 }
--- api/src/main/java/org/gitmining/api/dao/RepositoryDao.java
+++ api/src/main/java/org/gitmining/api/dao/RepositoryDao.java
@@ -1,6 +1,8 @@
 package org.gitmining.api.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,8 @@ public class RepositoryDao {
 	}
 	
 	public Document getRepo(String reponame){
-		return null;
+		Map<String, Object> filter = new HashMap<String, Object>();
+		filter.put("full_name", reponame);
+		return mongoQuery.searchOne(MongoInfo.DB, MongoInfo.REPO_COLLECTION, filter);
 	}
 }
